5716b663cac6ef489a4a589ff85c54136abd0ebb
vaibhav-tr98 <vaibhavtripathi99a@gmail.com>
2026-09-10 10:58:52 +0530

    fix: allow multipart resume uploads

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -13,6 +13,15 @@ api.interceptors.request.use(
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
+
+    if (config.data instanceof FormData) {
+      if (config.headers && typeof config.headers.delete === 'function') {
+        config.headers.delete('Content-Type');
+      } else {
+        delete config.headers['Content-Type'];
+      }
+    }
+
     return config;
   },
   (error) => Promise.reject(error)
